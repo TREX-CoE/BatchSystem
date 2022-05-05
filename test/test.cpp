828a3699927b513aee51b1f520ca92da4a6d5c3d
Kevin Thomas <kevin.thomas@megware.com>
2022-05-05 16:18:09 +0200

    add tests

--- test/test.cpp
+++ test/test.cpp
@@ -11,6 +11,8 @@
 
 using namespace std::placeholders;
 
+const std::string TESTDATA_DIR = std::string(TEST_DIR);
+
 namespace {
 
 int runCommand(const std::string& rootdir, std::stringstream& stream, const std::string& cmd, const std::vector<std::string>& args) {
@@ -44,16 +46,18 @@ TEST_CASE("Test system") {
         using namespace cw::batch;
         BatchSystem batchSystem;
 
-        slurm::create_batch(batchSystem, std::bind(runCommand, std::string(TEST_DIR)+"/", _1, _2, _3));
+        slurm::create_batch(batchSystem, std::bind(runCommand, TESTDATA_DIR+"/1/", _1, _2, _3));
+
+        std::vector<Job> jobs;
+        batchSystem.getJobs([&](Job j){ jobs.push_back(j); return true; });
+        CHECK(jobs.size() == 5);
+        CHECK(jobs[0].id.get()=="4");
 
         std::vector<std::string> lines;
-        batchSystem.getJobs([&](Job j){
-            lines.push_back(cw::batch::json::serialize(j));
-            return true;
-            //CBatchCatcher::stringSerialize(j, [&](auto s){lines.push_back(s);});
-        });
-        for (const auto& line : lines) {
-            std::cout << line << std::endl;
+        for (const auto& j : jobs) lines.push_back(json::serialize(j));
+
+        for (const auto& l : lines) {
+            std::cout << l << std::endl;
         }
     }
 }
